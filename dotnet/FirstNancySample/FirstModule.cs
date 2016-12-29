eca23172ef8c91751f3a81a49d9ccc286c27d9bf
Sergio Navarro Pino <Sergio.NavarroPino@gmail.com>
2016-12-29 19:21:21 +0100

    First commit with initial samples

---
+++ dotnet/FirstNancySample/FirstModule.cs
@@ -0,0 +1,17 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstNancySample
+{
+    public class FirstModule: NancyModule
+    {
+        public FirstModule()
+        {
+            Get["/"] = _ => "I did it my way!";
+            Get["/dashboard"] = _ => View["dashboard.html"];
+        }
+    }        
+}
